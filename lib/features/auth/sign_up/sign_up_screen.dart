a88227aba7896ca7ef89add4f608178de50eb04f
ekyza <ekyzahahah@gmail.com>
2023-12-22 13:40:49 +0700

    add supabase

---
+++ lib/features/auth/sign_up/sign_up_screen.dart
@@ -0,0 +1,203 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../../../providers/auth_provider.dart';
+import '../sign_in/sign_in_screen.dart';
+
+class SignUpScreen extends StatefulWidget {
+  const SignUpScreen({super.key});
+
+  @override
+  State<SignUpScreen> createState() => _SignUpScreenState();
+}
+
+class _SignUpScreenState extends State<SignUpScreen> {
+  TextEditingController username = TextEditingController();
+  TextEditingController email = TextEditingController();
+  TextEditingController password = TextEditingController();
+
+  @override
+  void dispose() {
+    username.dispose();
+    email.dispose();
+    password.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<AuthProvider>(builder: (context, authProvider, child) {
+      return Scaffold(
+        backgroundColor: const Color(0xffEDF6FF),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(40),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                const SizedBox(height: 50),
+                Image.asset(
+                  "assets/images/login.png",
+                  height: 240,
+                ),
+                const SizedBox(height: 16),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      "SIGN UP",
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontFamily: 'Vollkorn',
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 10),
+                TextField(
+                  controller: username,
+                  decoration: InputDecoration(
+                    labelText: 'Username',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      borderSide: BorderSide.none,
+                    ),
+                    filled: true,
+                    fillColor: Colors.white,
+                    contentPadding: const EdgeInsets.symmetric(
+                      vertical: 15,
+                      horizontal: 20,
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 25),
+                TextField(
+                  controller: email,
+                  decoration: InputDecoration(
+                    labelText: 'Email',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      borderSide: BorderSide.none,
+                    ),
+                    filled: true,
+                    fillColor: Colors.white,
+                    contentPadding: const EdgeInsets.symmetric(
+                      vertical: 15,
+                      horizontal: 20,
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 25),
+                TextField(
+                  controller: password,
+                  obscureText: true, // Untuk menyembunyikan teks (password)
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      borderSide: BorderSide.none,
+                    ),
+                    filled: true,
+                    fillColor: Colors.white,
+                    contentPadding: const EdgeInsets.symmetric(
+                      vertical: 15,
+                      horizontal: 20,
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 40),
+                TextButton(
+                  onPressed: () {
+                    authProvider.signUp(
+                      email: email.text,
+                      password: password.text,
+                    );
+                  },
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.resolveWith<Color?>(
+                      (Set<MaterialState> states) {
+                        if (states.contains(MaterialState.pressed)) {
+                          return const Color(0xff5F54A3);
+                        }
+                        return const Color(0xff6FA6FF);
+                      },
+                    ),
+                    padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+                      const EdgeInsets.symmetric(
+                        horizontal: 80,
+                        vertical: 10,
+                      ),
+                    ),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                  ),
+                  child: const Text(
+                    "SIGNUP",
+                    style: TextStyle(
+                      color: Color(0xffFDFDFD),
+                      fontSize: 20,
+                      fontFamily: 'Vollkorn',
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    const Text(
+                      "Have an account?",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 13,
+                        fontFamily: 'Vollkorn',
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) {
+                            return const SignInScreen();
+                          }),
+                        );
+                      },
+                      child: const Text(
+                        "Login",
+                        style: TextStyle(
+                          color: Color(0xffF75050),
+                          fontSize: 13,
+                          fontFamily: 'Vollkorn',
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      );
+    });
+  }
+}
---
+++ lib/features/boarding/boarding_screen.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+import '../../widgets/boarding_content.dart';
+import '../auth/sign_in/sign_in_screen.dart';
+
+final List<Widget> _pages = [
+  const BoardingContent(
+    imagePath: "assets/images/boarding1.png",
+    title: "ArithmoQues",
+    description:
+        "Welcome to ArithmoQuest! Prepare to embark on an exhilarating journey through the realm of numbers. Solve puzzles, conquer challenges, and ascend the MathLadder to prove your arithmetic prowess!",
+  ),
+  const BoardingContent(
+    imagePath: "assets/images/boarding2.png",
+    title: "Numeric Ascent: The MathLadder Adventure",
+    description:
+        "Numeric Ascent beckons! Join this thrilling adventure and navigate through mathematical challenges at every step. Climb the MathLadder, face numerical trials, and emerge as a math conqueror!",
+  ),
+  const BoardingContent(
+    imagePath: "assets/images/boarding3.png",
+    title: "MathMystic: Journey through Numbers",
+    description:
+        "Enter the world of MathMystic, where magic meets numbers. Explore the enchanted realm of mathematical puzzles, unlock secrets, and ascend the MathLadder to unveil the mysteries hidden within numerical calculations.",
+  ),
+];
+
+class BoardingScreen extends StatefulWidget {
+  const BoardingScreen({super.key});
+
+  @override
+  State<BoardingScreen> createState() => _BoardingScreenState();
+}
+
+class _BoardingScreenState extends State<BoardingScreen> {
+  final PageController _pageController = PageController(initialPage: 0);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView.builder(
+        controller: _pageController,
+        itemCount: _pages.length,
+        itemBuilder: (BuildContext context, int index) {
+          return _pages[index];
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          if (_pageController.page != _pages.length - 1) {
+            // Jika belum sampai halaman terakhir, pindah ke halaman berikutnya
+            _pageController.nextPage(
+              duration: const Duration(milliseconds: 500),
+              curve: Curves.ease,
+            );
+          } else {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) {
+                return const SignInScreen();
+              }),
+            );
+          }
+        },
+        child: const Text(
+          "Next",
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 16,
+            fontFamily: 'Vollkorn',
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
