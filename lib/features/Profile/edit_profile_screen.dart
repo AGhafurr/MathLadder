c7eb266a0e00295435df27d1744ef55c7c8daaed
AGhafurr <aghafurabdul@gmail.com>
2023-12-29 20:58:18 +0700

    Success

--- lib/features/Profile/edit_profile_screen.dart
+++ lib/features/Profile/edit_profile_screen.dart
@@ -27,92 +27,148 @@ class _EditProfileState extends State<EditProfile> {
           child: Column(
             children: [
               const SizedBox(height: 90),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                      color: Colors.white, // Ganti dengan warna yang diinginkan
+                    ),
+                    child: IconButton(
+                      icon: const Icon(
+                        Icons.arrow_back_ios_outlined,
+                        color: Colors.blue,
+                      ),
+                      iconSize: 36,
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      color: Colors.black,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 30),
               Stack(
-                alignment: Alignment.bottomRight,
+                alignment: Alignment.center,
                 children: [
                   GestureDetector(
                     onTap: () async {
                       await editProfileController.getImage(ImageSource.camera);
                     },
-                    child: Obx(() {
-                      return editProfileController.image.value != null
-                          ? ClipRRect(
-                              borderRadius: BorderRadius.circular(100),
-                              child: Image.file(
-                                editProfileController.image.value!,
-                                width: 250,
-                                height: 250,
-                              ),
-                            )
-                          : const Text('Pick Your Gorgeous Picture');
-                    }),
+                    child: Container(
+                      width: 230,
+                      height: 230,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        border: Border.all(
+                          color: const Color(0xffEDF6FF),
+                          width: 3, // Ketebalan border
+                        ),
+                      ),
+                      child: Obx(() {
+                        return editProfileController.image.value != null
+                            ? ClipRRect(
+                                borderRadius: BorderRadius.circular(100),
+                                child: Image.file(
+                                  editProfileController.image.value!,
+                                  width: 250,
+                                  height: 250,
+                                ),
+                              )
+                            : Container();
+                      }),
+                    ),
                   ),
-                  IconButton(
-                    onPressed: () {
-                      _showImageSourceDialog();
-                    },
-                    icon: const Icon(
-                      Icons.camera_alt,
-                      color: Colors.blue,
-                      size: 40,
+                  Positioned(
+                    top: 180,
+                    left: 150,
+                    child: GestureDetector(
+                      onTap: () {
+                        _showImageSourceDialog();
+                      },
+                      child: Container(
+                        padding: const EdgeInsets.all(8),
+                        decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.white,
+                        ),
+                        child: Image.asset(
+                          'assets/images/camera.png',
+                          width: 30,
+                          height: 30,
+                          color: Colors.blue,
+                        ),
+                      ),
                     ),
                   ),
                 ],
               ),
-              const SizedBox(height: 20),
-              const Text(
-                'Username',
-                style: TextStyle(
-                  fontSize: 15,
-                  fontFamily: 'Vollkorn',
-                ),
-                textAlign: TextAlign.left,
-              ),
-              const SizedBox(height: 8),
-              TextFormField(
-                controller: userNameController,
-                decoration: InputDecoration(
-                  hintText: 'Username',
-                  labelText: 'Username',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
+              const SizedBox(height: 50),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 35),
+                child: TextFormField(
+                  controller: userNameController,
+                  decoration: InputDecoration(
+                    hintText: 'Username',
+                    labelText: 'Change Name',
+                    filled: true, // Mengaktifkan background berwarna
+                    fillColor: Colors.white, // Warna background
+                    border: InputBorder.none, // Menghilangkan border
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide.none,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide.none,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    contentPadding: const EdgeInsets.symmetric(
+                        vertical: 14,
+                        horizontal: 12), // Atur ukuran padding konten
                   ),
+                  keyboardType: TextInputType.text,
                 ),
-                keyboardType: TextInputType.text,
               ),
               const SizedBox(height: 50),
-              ElevatedButton(
-                style: ButtonStyle(
-                  backgroundColor:
-                      MaterialStateProperty.all<Color>(const Color(0xff6FA6FF)),
-                  side: MaterialStateProperty.all(
-                      const BorderSide(color: Color(0xff01294D), width: 1)),
-                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                    RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(5),
-                    ),
-                  ),
-                  padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
-                    const EdgeInsets.symmetric(horizontal: 25, vertical: 10),
-                  ),
-                  elevation: MaterialStateProperty.all<double>(5),
-                ),
+              TextButton(
                 onPressed: () {
                   editProfileController.updateProfile(
                     id: authController.user['id'],
                     username: userNameController.text,
                   );
                 },
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.resolveWith<Color?>(
+                    (Set<MaterialState> states) {
+                      if (states.contains(MaterialState.pressed)) {
+                        return const Color(0xff5F54A3);
+                      }
+                      return const Color(0xff6FA6FF);
+                    },
+                  ),
+                  padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+                    const EdgeInsets.symmetric(
+                      horizontal: 80,
+                      vertical: 10,
+                    ),
+                  ),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                  ),
+                ),
                 child: const Text(
-                  "Submit",
+                  "Edit Profil",
                   style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 16,
+                    color: Color(0xffFDFDFD),
+                    fontSize: 20,
                     fontFamily: 'Vollkorn',
                   ),
                 ),
               ),
-              const SizedBox(height: 10),
             ],
           ),
         ),
@@ -125,28 +181,79 @@ class _EditProfileState extends State<EditProfile> {
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
-          title: const Text('Select Image Source'),
-          content: SingleChildScrollView(
-            child: ListBody(
-              children: [
-                GestureDetector(
-                  onTap: () {
-                    editProfileController.getImage(ImageSource.camera);
-                    Navigator.pop(context);
-                  },
-                  child: const Text('Camera'),
-                ),
-                const SizedBox(height: 20),
-                GestureDetector(
-                  onTap: () {
-                    editProfileController.getImage(ImageSource.gallery);
-                    Navigator.pop(context);
-                  },
-                  child: const Text('Gallery'),
-                ),
-              ],
-            ),
+          title: const Text(
+            'Image Source',
+            textAlign: TextAlign.center,
           ),
+          content: SingleChildScrollView(
+              child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextButton(
+                onPressed: () {
+                  editProfileController.getImage(ImageSource.camera);
+                  Navigator.pop(context);
+                },
+                style: ButtonStyle(
+                  padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+                    const EdgeInsets.all(10),
+                  ),
+                  backgroundColor: MaterialStateProperty.resolveWith<Color?>(
+                    (Set<MaterialState> states) {
+                      if (states.contains(MaterialState.pressed)) {
+                        return const Color(0xffFFFFFF);
+                      }
+                      return const Color(0xff6FA6FF);
+                    },
+                  ),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                  ),
+                ),
+                child: const Text(
+                  'Camera',
+                  style: TextStyle(
+                    fontSize: 15,
+                    color: Colors.white,
+                  ),
+                ), // Ganti teks sesuai kebutuhan
+              ),
+              const SizedBox(width: 20),
+              TextButton(
+                onPressed: () {
+                  editProfileController.getImage(ImageSource.gallery);
+                  Navigator.pop(context);
+                },
+                style: ButtonStyle(
+                  padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+                    const EdgeInsets.all(10),
+                  ),
+                  backgroundColor: MaterialStateProperty.resolveWith<Color?>(
+                    (Set<MaterialState> states) {
+                      if (states.contains(MaterialState.pressed)) {
+                        return const Color(0xffFFFFFF);
+                      }
+                      return const Color(0xff6FA6FF);
+                    },
+                  ),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                  ),
+                ),
+                child: const Text(
+                  'Galerry',
+                  style: TextStyle(
+                    fontSize: 15,
+                    color: Colors.white,
+                  ),
+                ), // Ganti teks sesuai kebutuhan
+              ),
+            ],
+          )),
         );
       },
     );
